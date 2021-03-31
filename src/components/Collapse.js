6627642abcbfa38b74be16f05e79ecf7016cd5f7
Fatih Cengiz <fatihce57@gmail.com>
2021-03-31 09:16:54 +0200

    state props

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,28 +1,56 @@
+// ilk constructor --> state initialization (ilk durum olusur)
+// render metodu ilk defa calisti
+// onClicck  iventi ile showMore calisti
+// setState state nesnesini güncelledi --> triggered render metodunu tekrar calistirdi
+// render metodu güncel JSX i sayfada gösteriyor
+
+
+
+
+
 import React from "react";
 
-const Collapse = (props) => {
-  console.log(props.children)  
-  return (
-    <div>
-      
-        <a
-          className="btn btn-primary"
-          data-bs-toggle="collapse"
-          href={"#".concat(props.href)}
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Link with href
-        </a>
-       
-    
-      <div className="collapse" id={props.href}>
-                 {props.children}
-       
+class Collapse extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showContent: false
+    }
+
+ /* this.showMore = () => {
+   console.log(this)
+ } 
+
+ this.showMore = this.showMore.bind(this) */
+  }
+
+ 
+
+  /* showMore() {
+    this.setState({showContent: true});
+  // console.log(this)
+  } */
+
+
+showMore = () => {
+  this.setState({showContent: !this.state.showContent})
+}
+
+
+
+  render() {
+   
+    return (
+      <div>
+        <button className="btn btn-primary w-100" onClick={this.showMore}>Link with href</button>
+
+        {this.state.showContent ? (
+          <div className="collapse show">{this.props.children}</div>
+        ) : null}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Collapse;
--- src/index.js
+++ src/index.js
@@ -4,39 +4,42 @@ import ReactDOM from "react-dom";
 import Card from "./components/Card";
 import Collapse from "./components/Collapse";
 
-
 const App = () => {
-
-  
   return (
-    <div>
-      <div className="card-group">
-        <Collapse href="collapseExample1" >
-          <Card
-            cardTitle="Card Titel 1"
-            cardText="Lorem Ipsum Text 1"
-            updatedTime="Last Updated 1 min ago"
-            image="https://picsum.photos/id/1/200/300"
-          />
-        </Collapse>
-
-        <Collapse href="collapseExample2" >
-          <Card
-            cardTitle="Card Titel 2"
-            cardText="Lorem Ipsum Text 2"
-            updatedTime="Last Updated 2 min ago"
-            image="https://picsum.photos/id/10/200/300"
-          />
-        </Collapse>
-
-        <Collapse href="collapseExample3" >
-          <Card
-            cardTitle="Card Titel 3"
-            cardText="Lorem Ipsum Text 3"
-            updatedTime="Last Updated 3 min ago"
-            image="https://picsum.photos/id/100/200/300"
-          />
-        </Collapse>
+    <div className="container">
+      <div className="row">
+        <div className="card-group w-100">
+          <div className="col">
+            <Collapse href="collapseExample1">
+              <Card
+               
+                cardText="Lorem Ipsum Text 1"
+                updatedTime="Last Updated 1 min ago"
+                image="https://picsum.photos/id/1/200/300"
+              />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample2">
+              <Card
+                cardTitle="Card Titel 2"
+                cardText="Lorem Ipsum Text 2"
+                updatedTime="Last Updated 2 min ago"
+                image="https://picsum.photos/id/2/200/300"
+              />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample3">
+              <Card
+             
+                cardText="Lorem Ipsum Text 3"
+                updatedTime="Last Updated 3 min ago"
+                image="https://picsum.photos/id/3/200/300"
+              />
+            </Collapse>
+          </div>
+        </div>
       </div>
     </div>
   );
